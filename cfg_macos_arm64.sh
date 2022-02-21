e2c84c30d925a0d4bd81e241e25cdaea28c697bd
christineyong <xtinewhy@gmail.com>
2022-02-21 19:55:02 +0800

    Remove redundant echoes

--- cfg_macos_arm64.sh
+++ cfg_macos_arm64.sh
@@ -2,4 +2,3 @@ CONDA_SUBDIR=osx-64 conda create -n or_training
 conda activate or_training
 conda env config vars set CONDA_SUBDIR=osx-64  # subsequent commands use intel packages
 conda env update --file environment.yml --prune
-echo "Re-start your shell and run 'conda activate or_training' to use CyLP and Cbc"
--- cfg_macos_intel.sh
+++ cfg_macos_intel.sh
@@ -1,3 +1,2 @@
 brew install cbc pkg-config
-conda create -n or_training -f environment.yml
-echo "Re-start your shell and run 'conda activate or_training' to use CyLP and Cbc"
+conda env create -f environment.yml
