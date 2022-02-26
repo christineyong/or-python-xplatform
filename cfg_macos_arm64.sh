c5c8d24b54fde68c1be9bee7d3efdca890cd379d
Christine Yong <75720165+christineyong@users.noreply.github.com>
2022-02-26 10:42:05 +0800

    Add install jupyter in base Rosetta environment

--- cfg_macos_arm64.sh
+++ cfg_macos_arm64.sh
@@ -1,7 +1,8 @@
 CONDA_SUBDIR=osx-64 conda create -n rosetta # create base environment using intel packages
 conda activate rosetta
 conda env config vars set CONDA_SUBDIR=osx-64  # subsequent commands use intel packages
-conda install nb_conda_kernels
+conda install nb_conda_kernels jupyter
+conda deactivate
 CONDA_SUBDIR=osx-64 conda create -n or_training
 conda activate or_training
 conda env config vars set CONDA_SUBDIR=osx-64  # subsequent commands use intel packages
