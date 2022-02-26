765b823723c3aa80d9653d38f629a6d8b20f7dff
Christine Yong <75720165+christineyong@users.noreply.github.com>
2022-02-26 10:43:50 +0800

    Add support for jupyter notebook

--- cfg_win32.bat
+++ cfg_win32.bat
@@ -1,3 +1,4 @@
 curl https://www.coin-or.org/download/binary/CoinAll/COIN-OR-1.7.4-win32-msvc11.exe -OutFile COIN-OR-1.7.4-win32-msvc11.exe
 .\COIN-OR-1.7.4-win32-msvc11.exe
+conda install nb_conda_kernels jupyter
 conda env create -f win_environment.yml
