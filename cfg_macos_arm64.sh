2a3f4dc06b3b30e02971dedf62c50bee68ab0ec2
Christine Yong <75720165+christineyong@users.noreply.github.com>
2022-02-26 10:39:32 +0800

    Add support for jupyter notebook

--- cfg_macos_arm64.sh
+++ cfg_macos_arm64.sh
@@ -1,3 +1,7 @@
+CONDA_SUBDIR=osx-64 conda create -n rosetta # create base environment using intel packages
+conda activate rosetta
+conda env config vars set CONDA_SUBDIR=osx-64  # subsequent commands use intel packages
+conda install nb_conda_kernels
 CONDA_SUBDIR=osx-64 conda create -n or_training
 conda activate or_training
 conda env config vars set CONDA_SUBDIR=osx-64  # subsequent commands use intel packages
