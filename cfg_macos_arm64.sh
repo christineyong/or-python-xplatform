8a48a6aab1a9e7de22789ff978078796dbad158d
Christine Yong <75720165+christineyong@users.noreply.github.com>
2022-02-26 10:52:18 +0800

    Fix jupyter install

--- cfg_macos_arm64.sh
+++ cfg_macos_arm64.sh
@@ -1,7 +1,7 @@
 CONDA_SUBDIR=osx-64 conda create -n rosetta # create base environment using intel packages
 conda activate rosetta
 conda env config vars set CONDA_SUBDIR=osx-64  # subsequent commands use intel packages
-conda install nb_conda_kernels jupyter
+conda install nb_conda_kernels jupyter notebook
 conda deactivate
 CONDA_SUBDIR=osx-64 conda create -n or_training
 conda activate or_training
